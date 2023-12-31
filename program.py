e90411f22c2a3d086ad04126b268e956ddc8c3c9
Alex Tran <60246286+Ampersand-Alexander@users.noreply.github.com>
2023-12-31 15:34:31 -0800

    threading, pytray icon, in preperation for exe
    bugs include: one thread running without the other, variable turn_on is not being registered in loop because it is not refreshing while the function is called,

--- program.py
+++ program.py
@@ -2,9 +2,18 @@ import serial
 import serial.tools.list_ports
 import time
 import keyboard 
+import pystray
+import threading
 
+from pystray import MenuItem as item
+from PIL import Image
 
-ser= None
+ser = None
+toggle_on = False
+
+def set_com(icon, item):
+    print("System tray icon clicked!")
+    pass
 
 def set_com():
     pass
@@ -24,27 +33,55 @@ def init():
     except RuntimeError:
         print("we tried to connect to the port, but it was blocked or already taken. Attempt Manual serial connection instead")
 
-def run(ser):
-    if ser is not None:
-        try:
-            while True:
-                key = keyboard.read_key()
-                if key != None:
-                    print(f'You pressed {key}')
-                    send_signal('1')
-                    time.sleep(.05)
-                    send_signal('0')
-                if key == "=": 
-                    print(f'Safe exit')
-                    send_signal('0')
-                    break
-        except KeyboardInterrupt:
-            pass
+def on_key_event(event):
+    if event.event_type == keyboard.KEY_DOWN:
+        print(f'You pressed')
+        send_signal('1')
+        time.sleep(.016)
+        send_signal('0')
 
-        finally:
-            ser.close()
-    else:
-        print("failed to connect to port")
+def run(ser, toggle_on):
+    print("thread 2")
+    while True:
+        if ser is not None and toggle_on:
+            try:
+                key = keyboard.read_key()
+                keyboard.hook(on_key_event)
+                keyboard.wait("")
+            except KeyboardInterrupt:
+                pass
+            finally:
+                keyboard.unhook_all()
+                ser.close()
+        elif ser is None:
+            print("failed to connect to port")
+
+def turn_on():
+    global toggle_on
+    toggle_on = True
+    print("turned on")
+
+def turn_off():
+    global toggle_on 
+    toggle_on = False
+    print("turned off")
+
+def quit_program():
+    menu = (item('Manually Adjust COM', set_com),item('Turn on', turn_on), item('Turn off', turn_off), item('Exit', quit_program), )
+    image = Image.open("C:\\Users\\Perfe\\ML\\middlemanarduino\\icon.png")
+    icon = pystray.Icon("Keyboard Solenoid", image, "Keyboard Solenoid", menu)
+    icon.stop()
+
+def run_icon():
+    print("thread 1")
+    menu = (item('Manually Adjust COM', set_com),item('Turn on', turn_on), item('Turn off', turn_off), item('Exit', quit_program), )
+    image = Image.open("C:\\Users\\Perfe\\ML\\middlemanarduino\\icon.png")
+    icon = pystray.Icon("Keyboard Solenoid", image, "Keyboard Solenoid", menu)
+    icon.run()
 
 ser=init()
-run(ser)
+Thread1 = threading.Thread(target=run_icon).start()
+Thread2 = threading.Thread(target=run(ser, toggle_on)).start()
+
+Thread1.join()
+Thread2.join()
